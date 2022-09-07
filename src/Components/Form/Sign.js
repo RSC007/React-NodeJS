c8fb2538f11f4b594fd8df4f71e63a8e3ec0d8fd
prathamesh patil <prathameshpatil2400@gmail.com>
2022-09-07 14:12:53 +0530

    Formik validation is added

--- src/Components/Form/Sign.js
+++ src/Components/Form/Sign.js
@@ -1,8 +1,44 @@
+import React from "react";
 import { Button, TextField } from "@mui/material";
 import Box from "@mui/material/Box";
-import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Sign = () => {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors: {
+      username: errUsername,
+      password: errPassword,
+      confirmPassword: errConfirmPassword
+    },
+    ...formikValues
+  } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+      confirmPassword: ""
+    },
+    onSubmit: (values) => {
+      console.log("Submit: ", values);
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required("User name is required."),
+      password: Yup.mixed().required("Password is required."),
+      confirmPassword: Yup.mixed()
+        .required("Password is not matched!")
+        .when("password", {
+          is: (val) => (val && val.length > 0 ? true : false),
+          then: Yup.mixed().oneOf(
+            [Yup.ref("password")],
+            "Both password need to be the same"
+          )
+        })
+    })
+  });
+
   return (
     <Box
       component="div"
@@ -29,18 +65,32 @@ export const Sign = () => {
           }
         }}
       >
-        <Box component='div' sx={{borderBottom: 2}}>Sign In Here!</Box>
+        <Box component="div" sx={{ borderBottom: 2 }}>
+          Sign In Here!
+        </Box>
+        <Box component='div'>
+          <TextField
+            error={!!errUsername}
+            id="username"
+            label="User Name"
+            variant="outlined"
+            sx={{
+              backgroundColor: "white",
+              color: "black",
+              margin: "3px"
+            }}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errUsername && (
+            <Box component="small" sx={{ color: "red", display: "block" }}>
+              {errUsername}
+            </Box>
+          )}
+        </Box>
+        <Box component='div'>
         <TextField
-          id="username"
-          label="User Name"
-          variant="outlined"
-          sx={{
-            backgroundColor: "white",
-            color: "black",
-            margin: "3px"
-          }}
-        />
-        <TextField
+          error={!!errPassword}
           id="password"
           label="Password"
           variant="outlined"
@@ -49,8 +99,18 @@ export const Sign = () => {
             color: "black",
             margin: "3px"
           }}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errPassword && (
+            <Box component="small" sx={{ color: "red", display: "block" }}>
+              {errPassword}
+            </Box>
+          )}
+        </Box>
+        <Box component='div'>
         <TextField
+          error={!!errConfirmPassword}
           id="confirmPassword"
           label="Confirm Password"
           variant="outlined"
@@ -59,10 +119,22 @@ export const Sign = () => {
             color: "black",
             margin: "3px"
           }}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errConfirmPassword && (
+            <Box component="small" sx={{ color: "red", display: "block" }}>
+              {errConfirmPassword}
+            </Box>
+          )}
+        </Box>
         <Box component="div">
           {/* <Button variant="contained">Sign In</Button> */}
-          <Button sx={{ paddingX: "20px" }} variant="contained">
+          <Button
+            onClick={handleSubmit}
+            sx={{ paddingX: "20px" }}
+            variant="contained"
+          >
             Log In
           </Button>
         </Box>
