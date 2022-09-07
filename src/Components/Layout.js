3273b0ac960dc4fbc5d6e674cb90af4bbcc8f584
prathamesh patil <prathameshpatil2400@gmail.com>
2022-09-07 11:48:48 +0530

    starter tamplate added

---
+++ src/Components/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Sign } from './Form/Sign'
+import './Style/Layout.css'
+
+export const Layout = () => {
+  return (
+    <div className='container'>
+      <Sign />
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Layout } from './Components/Layout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Layout/>
+  </React.StrictMode>
+);
